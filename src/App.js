ee9f483da2184362c001acbbcd7ea3e36c051a4c
william taylor <wt.william.taylor121@gmail.com>
2024-06-27 16:27:06 +0800

    src

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { AuthContextProvider } from "./context/AuthContext";
+import Home from "./pages/Home";
+import LogIn from "./pages/LogIn";
+import SignUp from "./pages/SignUp";
+import Account from "./pages/Account";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function App() {
+  return (
+    <>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route
+            path="/account"
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthContextProvider>
+    </>
+  );
+}
+
+export default App;
